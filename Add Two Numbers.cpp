682023b2435f4acdf2138625993511080eacf6e1
Sanjan <sanjangeet2109s@gmail.com>
2020-07-01 17:25:34 +0530

    Added Add Two Numbers

---
+++ Add Two Numbers.cpp
@@ -0,0 +1,16 @@
+/**
+ * Program to add two numbers.
+ * Author: Sanjan Geet Singh <sanjangeet2109s@gmail.com>
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    double x, y;
+    cout << "Enter X & Y: ";
+    cin >> x >> y;
+
+    cout << "Sum is " << x+y << endl;
+    return 0;
+}
