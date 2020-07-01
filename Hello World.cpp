bc475480a27398ca8d5f4c5a021ce66f1717a20d
Sanjan <sanjangeet2109s@gmail.com>
2020-07-01 17:26:49 +0530

    Added Hello World

---
+++ Hello World.cpp
@@ -0,0 +1,12 @@
+/**
+ * Program to print "Hello World!" on the screen without quotes.
+ * Author: Sanjan Geet Singh <sanjangeet2109s@gmail.com>
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!" << endl;
+	return 0;
+}
